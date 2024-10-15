ed552276c6004e81d6b63d807f9f8dd6a6d7e171
JohanDelao <johandelao10@gmail.com>
2024-10-14 21:44:07 -0400

    changing .env naming to fix

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
-import { PUBLIC_PROJECT_URL, PUBLIC_ANON_KEY } from '$env/dynamic/public'
 
-export const supabase = createClient(PUBLIC_PROJECT_URL, PUBLIC_ANON_KEY);
+export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
